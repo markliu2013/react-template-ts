d73b99610529b1bc9f816d67885c5c58887461cf
markliu2013 <markliu2013@gmail.com>
2024-02-14 13:33:40 +0800

    babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.ts',
@@ -7,24 +7,30 @@ module.exports = {
         path: path.join(__dirname, '/dist'), // the bundle output path
         filename: 'main.js' // the name of the bundle
     },
+    // import App from './App';
+    // import的时候不用加后缀
+    resolve: {
+        modules: [__dirname, 'src', 'node_modules'],
+        extensions: ['*', '.js', '.jsx', '.tsx', '.ts']
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: 'src/index.html' // to import index.html file inside index.js
         })
     ],
-    resolve: {
-        extensions: ['.js', '.ts', '.jsx', '.tsx', '.css']
-    },
     devServer: {
         port: 3035 // you can change the port
     },
     module: {
         rules: [
             {
-                test: /\.(ts|tsx)$/, // .js and .jsx files
+                test: /\.(js|ts)x?$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'ts-loader'
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
                 }
             }
         ]
