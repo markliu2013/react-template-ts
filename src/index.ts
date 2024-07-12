723a8e54d364b972f0addafaf3ddf8c5ccb79245
markliu2013 <markliu2013@gmail.com>
2024-07-12 11:50:47 +0800

    save

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 /*
  * https://stackoverflow.com/questions/41897420/typescript-and-reactdom-render-method-doesnt-accept-component
  * https://juejin.cn/post/7069598156735905800
- * import React from 'react'
  */
+// import React from 'react';
 import * as React from 'react';
 
 import { createRoot } from 'react-dom/client';
--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,34 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.ts',
-    output: {
-        path: path.resolve(__dirname, 'dist'), // the bundle output path
-        filename: 'main.js' // the name of the bundle
-    },
-    // import App from './App';
-    // import的时候不用加后缀
-    resolve: {
-        modules: [__dirname, 'src', 'node_modules'],
-        extensions: ['*', '.js', '.jsx', '.tsx', '.ts']
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html' // to import index.html file inside index.js
-        })
-    ],
-    devServer: {
-        port: 3035 // you can change the port
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|ts)x?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            }
-        ]
-    }
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'), // the bundle output path
+    filename: 'main.js' // the name of the bundle
+  },
+  // import App from './App';
+  // import的时候不用加后缀
+  resolve: {
+    modules: [__dirname, 'src', 'node_modules'],
+    extensions: ['*', '.js', '.jsx', '.tsx', '.ts']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html' // to import index.html file inside index.js
+    })
+  ],
+  devServer: {
+    port: 3035 // you can change the port
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader'
+        }
+      }
+    ]
+  }
 };
